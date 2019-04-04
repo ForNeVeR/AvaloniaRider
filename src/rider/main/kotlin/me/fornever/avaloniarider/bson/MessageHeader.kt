86f81fbf57639fa99436e3fb75430d7ce24daf18
maryilyina <maryilyina96@gmail.com>
2019-04-04 20:45:01 +0700

    Implement Avalonia message serialization with test

--- src/rider/main/kotlin/me/fornever/avaloniarider/bson/MessageHeader.kt
+++ src/rider/main/kotlin/me/fornever/avaloniarider/bson/MessageHeader.kt
@@ -1,10 +1,12 @@
 package me.fornever.avaloniarider.bson
 
+import com.intellij.util.io.toByteArray
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.util.*
 
 data class MessageHeader(val length: Int, val typeId: UUID) {
+
     companion object {
         fun fromBytes(bytes: ByteArray): MessageHeader {
             val buffer = ByteBuffer.wrap(bytes).order(ByteOrder.nativeOrder())
@@ -26,5 +28,27 @@ data class MessageHeader(val length: Int, val typeId: UUID) {
             val messageId = readDotNetGuid()
             return MessageHeader(length, messageId)
         }
+    }
+
+    fun toByteArray(): ByteArray {
+        val long1 = typeId.mostSignificantBits
+        val long2 = typeId.leastSignificantBits
+
+        val buffer = ByteBuffer.allocate(8)
+        buffer.putLong(long1)
+        buffer.position(0)
+        val a = buffer.order(ByteOrder.LITTLE_ENDIAN).int
+        val b = buffer.short
+        val c = buffer.short
+
+        return ByteBuffer.allocate(20).apply {
+            order(ByteOrder.LITTLE_ENDIAN)
+            putInt(length)
+            order(ByteOrder.BIG_ENDIAN)
+            putInt(a)
+            putShort(b)
+            putShort(c)
+            putLong(long2)
+        }.toByteArray()
     }
 }
--- src/test/kotlin/bson/MessageHeaderTests.kt
+++ src/test/kotlin/bson/MessageHeaderTests.kt
@@ -5,14 +5,23 @@ import org.testng.annotations.Test
 import java.util.*
 
 class MessageHeaderTests {
+    val byteData = intArrayOf(0x39, 0x0, 0x0, 0x0,
+            0xCF, 0x87, 0x48, 0x85, 0x94, 0x26, 0xB6, 0x4E,
+            0xB4, 0x99, 0x74, 0x61, 0xB6, 0xFB, 0x96, 0xC7)
+            .map { it.toByte() }
+            .toByteArray()
+    val guid = UUID.fromString("854887CF-2694-4EB6-B499-7461B6FB96C7")
+
     @Test
     fun testDeserialization() {
-        val data = intArrayOf(0x39, 0x0, 0x0, 0x0,
-                0xCF, 0x87, 0x48, 0x85, 0x94, 0x26, 0xB6, 0x4E,
-                0xB4, 0x99, 0x74, 0x61, 0xB6, 0xFB, 0x96, 0xC7)
-                .map { it.toByte() }
-                .toByteArray()
-        val expected = MessageHeader(57, UUID.fromString("854887CF-2694-4EB6-B499-7461B6FB96C7"))
-        MessageHeader.fromBytes(data).shouldBe(expected)
+        val expected = MessageHeader(57, guid)
+        MessageHeader.fromBytes(byteData).shouldBe(expected)
+    }
+
+    @Test
+    fun testSerialization() {
+        val header = MessageHeader(57, guid)
+        val res = header.toByteArray()
+        res.shouldBe(byteData)
     }
 }
