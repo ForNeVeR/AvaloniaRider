d251badbfad5d54aeddc558bea96f302905ac6bf
Friedrich von Never <friedrich@fornever.me>
2020-02-02 20:54:34 +0700

    Extract the previewer editor component (#17)

--- src/rider/main/kotlin/idea/editor/AvaloniaPreviewEditor.kt
+++ src/rider/main/kotlin/idea/editor/AvaloniaPreviewEditor.kt
@@ -6,14 +6,12 @@ import com.intellij.openapi.fileEditor.FileEditorLocation
 import com.intellij.openapi.fileEditor.FileEditorState
 import com.intellij.openapi.util.UserDataHolderBase
 import java.beans.PropertyChangeListener
-import javax.swing.JLabel
-import javax.swing.JPanel
 
 class AvaloniaPreviewEditor : UserDataHolderBase(), FileEditor {
     override fun getName() = "Previewer"
     override fun isValid() = true
 
-    private val panel = lazy { JPanel().apply { add(JLabel("Avalonia Previewer Placeholder")) } }
+    private val panel = lazy { AvaloniaPreviewEditorComponent() }
 
     override fun getComponent() = panel.value
     override fun getPreferredFocusedComponent() = component
---
+++ src/rider/main/kotlin/idea/editor/AvaloniaPreviewEditorComponent.kt
@@ -0,0 +1,10 @@
+package me.fornever.avaloniarider.idea.editor
+
+import javax.swing.JLabel
+import javax.swing.JPanel
+
+class AvaloniaPreviewEditorComponent : JPanel() {
+    init {
+        add(JLabel("Avalonia Previewer Placeholder"))
+    }
+}
