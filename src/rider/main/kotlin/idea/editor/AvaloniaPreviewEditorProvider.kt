c0a4d6db6598b80c82ed6cd43bfbd2655c9157b2
Friedrich von Never <friedrich@fornever.me>
2020-02-02 21:21:37 +0700

    Replace the default XAML editor with a hybrid previewer (#17)

--- src/rider/main/kotlin/idea/editor/AvaloniaPreviewEditorProvider.kt
+++ src/rider/main/kotlin/idea/editor/AvaloniaPreviewEditorProvider.kt
@@ -1,19 +1,19 @@
 package me.fornever.avaloniarider.idea.editor
 
-import com.intellij.openapi.fileEditor.FileEditor
-import com.intellij.openapi.fileEditor.FileEditorPolicy
-import com.intellij.openapi.fileEditor.FileEditorProvider
+import com.intellij.openapi.fileEditor.*
+import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 
 class AvaloniaPreviewEditorProvider : FileEditorProvider {
     override fun getEditorTypeId() = "AvaloniaPreviewerEditor"
-    override fun getPolicy() = FileEditorPolicy.PLACE_AFTER_DEFAULT_EDITOR
+    override fun getPolicy() = FileEditorPolicy.HIDE_DEFAULT_EDITOR
 
     override fun accept(project: Project, file: VirtualFile) =
             file.extension == "xaml" // TODO: Backend XAML file check
 
     override fun createEditor(project: Project, file: VirtualFile): FileEditor {
-        return AvaloniaPreviewEditor()
+        val textEditor = TextEditorProvider.getInstance().createEditor(project, file) as TextEditor
+        return TextEditorWithPreview(textEditor, AvaloniaPreviewEditor())
     }
 }
