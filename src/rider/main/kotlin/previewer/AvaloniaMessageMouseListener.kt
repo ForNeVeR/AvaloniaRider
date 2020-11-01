b325752181250a3dfc5ed79c2fb2fedb58b8f442
Rustam Sayfutdinov <rstm.sf@gmail.com>
2020-11-01 09:49:56 +0300

    Clarify in doc why need JLabel instance

--- src/rider/main/kotlin/previewer/AvaloniaMessageMouseListener.kt
+++ src/rider/main/kotlin/previewer/AvaloniaMessageMouseListener.kt
@@ -15,7 +15,9 @@ import javax.swing.JLabel
 import javax.swing.SwingConstants
 import javax.swing.event.MouseInputAdapter
 
-
+/**
+ * @suppress Pass the JLabel instance in order to find out the position of the pointer to the Icon
+ */
 internal class AvaloniaMessageMouseListener(
     private val frameView: JLabel
 ) : MouseInputAdapter() {
