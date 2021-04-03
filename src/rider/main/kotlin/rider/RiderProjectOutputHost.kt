eb904b44d3500716214af02e627dd1a26afbe0ae
Friedrich von Never <friedrich@fornever.me>
2021-04-03 17:37:40 +0700

    #125: get rid of outdated dispatchers

--- src/rider/main/kotlin/rider/RiderProjectOutputHost.kt
+++ src/rider/main/kotlin/rider/RiderProjectOutputHost.kt
@@ -6,12 +6,10 @@ import com.intellij.workspaceModel.ide.toPath
 import com.jetbrains.rd.ide.model.RdGetProjectOutputArgs
 import com.jetbrains.rd.ide.model.RdProjectOutput
 import com.jetbrains.rd.ide.model.riderProjectOutputModel
+import com.jetbrains.rd.platform.util.withUiContext
 import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rider.projectView.solution
 import com.jetbrains.rider.projectView.workspace.ProjectModelEntity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import me.fornever.avaloniarider.idea.concurrency.ApplicationAnyModality
 import me.fornever.avaloniarider.idea.concurrency.await
 
 @Service
@@ -22,7 +20,7 @@ class RiderProjectOutputHost(private val project: Project) {
     }
 
     suspend fun getProjectOutput(lifetime: Lifetime, projectNode: ProjectModelEntity): RdProjectOutput =
-        withContext(Dispatchers.ApplicationAnyModality) {
+        withUiContext(lifetime) {
             val model = project.solution.riderProjectOutputModel
             @Suppress("UnstableApiUsage") val projectFilePath = projectNode.url!!.toPath().toString()
             model.getProjectOutput.start(lifetime, RdGetProjectOutputArgs(projectFilePath)).await(lifetime)
