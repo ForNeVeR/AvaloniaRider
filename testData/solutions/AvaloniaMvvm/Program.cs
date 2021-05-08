c5bc91e4797ef15131b141b37dcd4a6cb367c934
Friedrich von Never <friedrich@fornever.me>
2021-05-08 21:42:54 +0700

    AvaloniaMvvm: test project cleanup
    This also removes accidental Cyrillic character from a comment; thanks
    to @Fantoom for noticing!

--- testData/solutions/AvaloniaMvvm/Program.cs
+++ testData/solutions/AvaloniaMvvm/Program.cs
@@ -8,13 +8,9 @@ namespace avalonia.mvvm.test
 {
     class Program
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
         public static void Main(string[] args) => BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
 
-        // Avalonia configuration, don'чt remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
