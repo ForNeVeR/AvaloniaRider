af9f49ecaccaaf8bea693c658b627774ed4dec1f
Friedrich von Never <friedrich@fornever.me>
2022-11-24 23:25:54 +0100

    Dependencies: update Rider to 2022.3 EAP7

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     id("java")
     id("me.filippov.gradle.jvm.wrapper") version "0.14.0"
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
-    id("org.jetbrains.intellij") version "1.9.0"
+    id("org.jetbrains.intellij") version "1.10.0"
 }
 
 apply {
