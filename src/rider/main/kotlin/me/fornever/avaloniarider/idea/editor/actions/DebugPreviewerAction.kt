b0bb6778eb8fffd6194fb2deb899bd4c9c572f79
Friedrich von Never <friedrich@fornever.me>
2024-08-29 00:00:12 +0200

    (#319) Debug Previewer: add an action skeleton

--- src/rider/main/kotlin/me/fornever/avaloniarider/idea/editor/actions/DebugPreviewerAction.kt
+++ src/rider/main/kotlin/me/fornever/avaloniarider/idea/editor/actions/DebugPreviewerAction.kt
@@ -1,12 +1,21 @@
 package me.fornever.avaloniarider.idea.editor.actions
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.jetbrains.rd.util.lifetime.Lifetime
+import kotlinx.coroutines.launch
+import me.fornever.avaloniarider.AvaloniaRiderBundle
 import me.fornever.avaloniarider.idea.settings.AvaloniaApplicationSettings
 
-class DebugPreviewerAction : AnAction() {
+class DebugPreviewerAction(private val lifetime: Lifetime) : AnAction(
+    AvaloniaRiderBundle.messagePointer("action.debug-previewer.text"),
+    AvaloniaRiderBundle.messagePointer("action.debug-previewer.description"),
+    AllIcons.Debugger.Console
+) {
     override fun getActionUpdateThread() = ActionUpdateThread.BGT
+
     override fun update(e: AnActionEvent) {
         val presentation = e.presentation
         if (!AvaloniaApplicationSettings.getInstance().isDeveloperModeEnabled) {
@@ -18,6 +27,9 @@ class DebugPreviewerAction : AnAction() {
     }
 
     override fun actionPerformed(p0: AnActionEvent) {
-
+         lifetime.coroutineScope.launch {
+             TODO("Get the debugger process startup command")
+             TODO("Start the debug session")
+         }
     }
 }
