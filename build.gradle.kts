916c23c75a2db5564040383f8d8aaccd2a122bae
Friedrich von Never <friedrich@fornever.me>
2025-10-23 17:03:15 +0200

    Build: exclude org.eclipse.jgit

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,6 +77,12 @@ dependencies {
     testImplementation(libs.testng)
 }
 
+configurations {
+    intellijPlatformTestDependencies {
+        exclude("org.jetbrains.intellij.deps", "org.eclipse.jgit")
+    }
+}
+
 val buildConfiguration = ext.properties["buildConfiguration"] as String? ?: "Debug"
 val buildNumber = (ext.properties["buildNumber"] as String?)?.toInt() ?: 0
 
