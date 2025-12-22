e4b3a9c758a2ffe628283498be293660d586c1b1
SuperJMN <superjmn@outlook.com>
2025-12-22 23:54:40 +0100

    Add the ability to detach previewer from the view-mode toolbar

--- src/rider/main/kotlin/me/fornever/avaloniarider/idea/editor/AvaloniaRemotePreviewEditor.kt
+++ src/rider/main/kotlin/me/fornever/avaloniarider/idea/editor/AvaloniaRemotePreviewEditor.kt
@@ -1,21 +1,27 @@
 package me.fornever.avaloniarider.idea.editor
 
+import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.rider.xaml.splitEditor.XamlSplitEditor
 import me.fornever.avaloniarider.idea.editor.actions.ZoomLevelSelectorAction
 import me.fornever.avaloniarider.idea.settings.AvaloniaProjectSettings
 import javax.swing.JComponent
 
 class AvaloniaRemotePreviewEditor(
     project: Project,
-    currentFile: VirtualFile
-) : AvaloniaPreviewEditorBase(project, currentFile) {
+    currentFile: VirtualFile,
+    parentEditor: XamlSplitEditor? = null
+) : AvaloniaPreviewEditorBase(project, currentFile, parentEditor) {
 
     private val panel = lazy {
         BitmapPreviewEditorComponent(lifetime, sessionController, AvaloniaProjectSettings.getInstance(project))
     }
 
+    private val zoomAction = ZoomLevelSelectorAction(sessionController.zoomFactor)
+
     override val editorComponent = panel.value
+    override fun getExtraActions(): Array<AnAction> = arrayOf(zoomAction)
     override fun createToolbar(targetComponent: JComponent) =
-        createToolbarComponent(targetComponent, ZoomLevelSelectorAction(sessionController.zoomFactor))
+        createToolbarComponent(targetComponent, false, zoomAction)
 }
